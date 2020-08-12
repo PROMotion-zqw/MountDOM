ff8b1379ea28144deeb6dfa21a536508eca31750
泉伟郑郑泉伟 <quanwei.zheng.o@nio.com>
2020-08-12 18:03:57 +0800

    ty_0.0.1

--- typesc/first.js
+++ typesc/first.js
@@ -1,5 +1,11 @@
-function start(person) {
-    return "Hello, " + person.firstName + " " + person.lastName;
+// 继承多个接口
+var square_m = {};
+var square = {};
+square.sideLength = 100;
+square.color = "red";
+function start(ps) {
+    console.log(ps.color, ps.sideLength);
 }
-var user = { firstName: 'Jane', lastName: 'User' };
-document.body.innerHTML = start(user);
+var par = { color: "red", name: 'quanwei', sideLength: 100 };
+console.log('square', square);
+start(par);
--- typesc/first.ts
+++ typesc/first.ts
@@ -1,11 +1,35 @@
-interface ps {
-    firstName: string;
-    lastName: string;
+// 继承接口
+interface Shape {
+    color: string;
 }
 
-function start (person: ps) {
-    return "Hello, "+person.firstName + " " + person.lastName;
+interface PenStroke {
+    penWdith: number;
 }
 
-var user = {firstName: 'Jane', lastName: 'User'};
-document.body.innerHTML = start(user);
+// 继承一个结构
+interface Square extends Shape {
+    sideLength: number;
+}
+
+interface Square_mul extends Shape, PenStroke {
+    sideLength: number;
+}
+
+// 继承多个接口
+let square_m = <Square_mul>{};
+square_m.penWdith = 200;
+square_m.color = "red";
+square_m.sideLength = 10;
+
+let square = <Square>{};
+square.sideLength = 100;
+square.color = "red";
+
+function start(ps: Square | Square_mul): void {
+    console.log(ps.color, ps.sideLength);
+}
+
+let par = {color: "red", name: 'quanwei', sideLength: 100}
+console.log('square', square);
+start(par)
